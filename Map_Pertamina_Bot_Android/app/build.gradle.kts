428c1b6c890e95fb0751c9adb36927857e3f0ae8
TheMask112 <dimasarifdilano@gmail.com>
2026-08-30 13:16:59 +0700

    chore(android): penyempurnaan aplikasi dan perbaikan bugs

--- Map_Pertamina_Bot_Android/app/build.gradle.kts
+++ Map_Pertamina_Bot_Android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mapbot.pertamina"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.1.7"
+        versionCode = 18
+        versionName = "1.1.8"
 
         vectorDrawables {
             useSupportLibrary = true
